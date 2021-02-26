9a3681c43484f178c4fd8793da8334d8583f2aba
utku berk gök <utku_19_berk@hotmail.com>
2021-02-26 14:31:32 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        //Değişken tanımladık
+        TKey[] keys;
+        TValue[] values;
+
+        //constructor bloğu(newlediğimizde çalışıcak olan blok) bir metotdur.
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        //işlem bloğumuz yani metodumuz.
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeysArray = keys;
+            keys = new TKey[keys.Length + 1];
+            
+
+            for (int i = 0; i < tempKeysArray.Length; i++)
+            {
+                keys[i] = tempKeysArray[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            TValue[] tempValuesArray = values;
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempValuesArray.Length; i++)
+            {
+                values[i] = tempValuesArray[i];
+            }
+            values[values.Length - 1] = value;
+        }
+        //ekrana yazdırma metotdumuz
+        public void List()
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine("Seri Numarası : " + keys[i] + " " + "Telefon Marka Ve Modeli : " + values[i]);
+            }
+        }
+    }
+    
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> products = new MyDictionary<int, string>();
+
+            products.Add(524781, "Iphone Pro 12");
+            products.Add(534864, "LG G4");
+            products.Add(536948, "Samsung Note 10");
+            products.Add(543897, "Xiaomi Note 9 Pro");
+
+            products.List();
+        }
+    }
+}
